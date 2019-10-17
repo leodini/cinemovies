5c14dd03f97bac152a8e6b55a80bc91a15dbd23b
Leonardo Dini <leodini@hotmail.com>
2019-10-17 03:32:25 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,57 @@
+import React, { useState, useEffect} from 'react';
+import './App.css';
+
+const api = process.env.API_KEY || 'be0fcdce8503d9bc8632f8ee275eb512';
+
+function App() {
+  const [ movie, setMovie ] = useState([{}]);
+  const [ name, setName ] = useState('');
+  
+
+  useEffect(() => {
+    moviesList();
+  }, [])
+
+  const getMovie = async(event) => {
+    event.preventDefault();
+    let api_call = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${api}&language=en-US&query=${name}&page=1&include_adult=false`);
+    let data = await api_call.json();
+    console.log(data);
+    setMovie(data);
+  }
+
+  const moviesList = async() => {
+    let api_call = await fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${api}&language=en-US`);
+    let data = await api_call.json();
+    
+    data.genres.map(async(i) => {
+      const id = i.id;
+      let lista = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${api}&language=en-US`);
+      let data = await lista.json();
+      console.log(data);
+    })
+    
+  }
+
+  return (
+    <div className="App">
+
+      <form onSubmit={getMovie} className="search">
+        <input type="text" placeholder="pesquise aqui seu filme favorito" required onChange={(event) => setName(event.target.value)}/>
+        <button type="submit">Pesquisar</button>
+      </form>
+
+      
+          <div className="">
+            <span>{movie.original_title}</span>
+            <span>{movie.overview}</span>
+            <span>{movie.vote_average}</span>
+          </div>
+        
+      
+      
+    </div>
+  );
+}
+
+export default App;
