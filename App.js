b5b30680f2dc8ab7f151f43cdac5b555eec54df4
Leonardo Dini <leodini@hotmail.com>
2019-10-24 02:41:01 -0300

    Update App.js

--- App.js
+++ App.js
@@ -7,53 +7,45 @@ function App() {
   const [ movie, setMovie ] = useState([{}]);
   const [ name, setName ] = useState('');
   
-
-  useEffect(() => {
-    moviesList();
+useEffect(() => {
+    const discover = async() =>{
+      const req = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${api}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`);
+      const res = await req.json();
+      setMovie(res.results);
+      console.log(res.results)
+    }
+    discover();
   }, [])
+
 
   const getMovie = async(event) => {
     event.preventDefault();
     let api_call = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${api}&language=en-US&query=${name}&page=1&include_adult=false`);
     let data = await api_call.json();
+    const results = data.results;
     setMovie(data.results);
-    console.log(data.results);
-    
-  }
-
-
-  const moviesList = async() => {
-    for(let i = 0; i <= 50; i++ ){
-      const teste = Math.floor(Math.random() * 450);
-      const lista = await fetch(`https://api.themoviedb.org/3/movie/${teste}?api_key=${api}&language=en-US`);
-      const data = await lista.json();
-      setMovieList({ data });
-    }
-    
+    results.forEach(movie => {
+      const movieRow = [];
+      movieRow.push(setMovie);
+      console.log(movie);
+    });
   }
 
   return (
     <div className="App">
-
+      <div className="header">
+      <h2 className="app-name">Cinemovies</h2>
+      
       <form onSubmit={getMovie} className="search">
-        <input type="text" placeholder="pesquise aqui seu filme favorito" required onChange={(event) => setName(event.target.value)}/>
+        <input className="input" required type="text" name="titulo" placeholder="pesquise aqui seu filme favorito" required onChange={(event) => setName(event.target.value)}/>
         <button type="submit">Pesquisar</button>
       </form>
-      {
-        movie.map(movies => {
-          return(
-            <ul key={movies.id}>
-              <li>{movies.original_title}</li>
-              <img src={`http://image.tmdb.org/t/p/w185/${movies.poster_path}`} alt="poster"/>
-            </ul>
-          )
-        })
-      }
-      
-      
-      
+      </div>
+      <MovieSearch movie={movie}/>
     </div>
   );
 }
+
+
 
 export default App;
